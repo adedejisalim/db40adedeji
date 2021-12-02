c3c24cb7b921f0c50589de543ce7dce0fd30c817
salim <s534027@nwmissouri.edu>
2021-12-01 19:13:50 -0600

    completed the module

--- models/food.js
+++ models/food.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose") 
 const foodSchema = mongoose.Schema({ 
- name: String, 
+ name: {
+    type: String,
+    required: true,
+ },
  color: String, 
  rating: Number 
 }) 
